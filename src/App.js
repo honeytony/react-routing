235b838ddbe712ae3841bf22cc172ed4e0cac721
Anton Kucher <antonddmedia@gmail.com>
2024-02-07 11:04:27 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import MainPage from './components/mainPage/mainPage';
+import ProfilePage from './components/profilePage/profilePage';
+import AboutPage from './components/aboutPage/aboutPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Routes>
+          <Route path='/' Component={MainPage} />
+          <Route path='/profile' Component={ProfilePage} />
+          <Route path='/about' Component={AboutPage} />
+        </Routes>
+        <Footer/>
     </div>
   );
 }
